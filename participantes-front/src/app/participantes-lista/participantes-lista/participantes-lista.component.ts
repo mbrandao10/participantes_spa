d95023afe4b9e53aa8a566c3dc90f909e012dce3
pedro.scattaregi@limberhealth.com <pedro.scattaregi@limberhealth.com>
2024-05-12 00:56:54 -0300

    feat: adicionado layout tabela e organizacao e imports angular material

--- participantes-front/src/app/participantes-lista/participantes-lista/participantes-lista.component.ts
+++ participantes-front/src/app/participantes-lista/participantes-lista/participantes-lista.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Participantes } from '../model/participantes';
+
 @Component({
   selector: 'app-participantes-lista',
   templateUrl: './participantes-lista.component.html',
   styleUrls: ['./participantes-lista.component.scss']
 })
 export class ParticipantesListaComponent implements OnInit {
+
+  participantes_lista: Participantes[] = [
+    { _id: '1', nome: 'matheus', cpf: '123456643', telefone: '12123423453', sexo: 'M' },
+    { _id: '1', nome: 'matheus', cpf: '123456643', telefone: '12123423453', sexo: 'M' },
+    { _id: '1', nome: 'matheus', cpf: '123456643', telefone: '12123423453', sexo: 'M' },
+    { _id: '1', nome: 'matheus', cpf: '123456643', telefone: '12123423453', sexo: 'M' },
+    { _id: '1', nome: 'matheus', cpf: '123456643', telefone: '12123423453', sexo: 'M' },
+    { _id: '1', nome: 'matheus', cpf: '123456643', telefone: '12123423453', sexo: 'M' }
+  ];
+  displayedColumns = ['nome', 'cpf', 'telefone', 'sexo']
 
   constructor() { }
 
